60fa4651dba0c498424ea187cf66e7a624c6852d
Jalal <jalal.ghani0100@gmail.com>
2022-06-23 15:41:27 +0500

    Individual salary half screen 23-06-2022

--- app/src/main/java/com/irs/ghani/caltax/MainActivity.java
+++ app/src/main/java/com/irs/ghani/caltax/MainActivity.java
@@ -2,13 +2,42 @@ package com.irs.ghani.caltax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.irs.ghani.caltax.individual.IndividualSalary;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mNext;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNext = findViewById(R.id.btn_home_next);
+        mNext.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btn_home_next:
+            {
+                intent = new Intent(MainActivity.this , IndividualSalary.class);
+                startActivity(intent);
+                break;
+            }
+        }
+    }
+
+    private void openActivity(Intent intent)
+    {
+        startActivity(intent);
     }
 }
